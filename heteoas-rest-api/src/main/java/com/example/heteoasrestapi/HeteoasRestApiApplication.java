417f5bec35ef48b16a1da46a2aa9c740f12ce057
baegseungchan-gram <mokdonjr@gmail.com>
2020-03-28 01:33:52 +0900

    [heteaos-rest-api] init spring boot project

---
+++ heteoas-rest-api/src/main/java/com/example/heteoasrestapi/HeteoasRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.heteoasrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeteoasRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeteoasRestApiApplication.class, args);
+	}
+
+}
---
+++ heteoas-rest-api/src/test/java/com/example/heteoasrestapi/HeteoasRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.heteoasrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeteoasRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
